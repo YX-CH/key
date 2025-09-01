a4b4d18ed5c627b4307f949c7d572fb7e519cf1b
z2663 <z2663462801z@163.com>
2025-09-01 12:31:50 +0800

    init

--- app/src/main/java/com/zero/keymanager/MainActivity.kt
+++ app/src/main/java/com/zero/keymanager/MainActivity.kt
@@ -1,29 +1,35 @@
 package com.zero.keymanager
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.zero.keymanager.ui.theme.KeyManagerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        this.window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
+
         enableEdgeToEdge()
         setContent {
             KeyManagerTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold {
+                    Box(Modifier.fillMaxSize().background(Color.Black)){
+                        Box (Modifier.padding(it).fillMaxSize().background(Color.Blue)){  }
+                    }
                 }
             }
         }
